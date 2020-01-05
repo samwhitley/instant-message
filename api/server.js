a3d01bb032b7d4d6ce92a386a8e5c07b751a8940
Sam Whitley <sam@samwhitley.io>
2020-01-05 14:37:25 -0500

    Make api server return a string of json data

--- api/server.js
+++ api/server.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
 
 const PORT = process.env.PORT || 9000;
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('API is working!');
+  const dataPath = path.resolve('./api/data.json');
+  const rawData = fs.readFileSync(dataPath);
+  const data = JSON.parse(rawData);
+
+  res.send(JSON.stringify(data));
 });
 
 app.listen(PORT, () => {
