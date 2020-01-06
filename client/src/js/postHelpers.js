eb98dc6b4264de8a8e8097af3db92f279097cc63
Sam Whitley <sam@samwhitley.io>
2020-01-05 21:00:23 -0500

    Add user data to posts

--- client/src/js/postHelpers.js
+++ client/src/js/postHelpers.js
@@ -1,20 +1,40 @@
 const timeHelpers = require('./timeHelpers');
 
-const makePost = post => {
+const getUsers = data => {
+  let users = {};
+
+  for (let i = 0; i < data.users.length; i++) {
+    let user = data.users[i];
+
+    users[user.id] = {
+      "username": user.username,
+      "real_name": user.real_name,
+      "verified": user.verified
+    }
+  }
+
+  return users;
+};
+
+const makePost = (post, user) => {
   const timeSince = `${timeHelpers.timeSince(post.ts)} ago`;
+  const userHeading = `${user.real_name} - @${user.username}`;
 
   return `
   <li>
+    <p>${userHeading}</p>
     <p>${timeSince}</p>
     <p>${post.message}</p>
   </li>`;
 };
 
 const makeList = data => {
+  const users = getUsers(data);
   let list = '<ul class="messages">';
 
   for (let i = 0; i < data.posts.length; i++) {
-    list += makePost(data.posts[i]);
+    let post = data.posts[i];
+    list += makePost(post, users[post.user]);
   }
 
   list += '</ul>';
